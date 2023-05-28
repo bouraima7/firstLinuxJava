0aa27325f56d14017ad5123728c2898d165ee746
bouraima <bouraima799@gmail.com>
2023-05-28 04:00:10 -0400

    First commit on Linux

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print("I am simply messing around with Java in a Linux environment");
+        System.out.print("I will look into how File input/output may be different on linux");
+        System.out.print("I will also look to see if it is ideal to build full stack projects on Linux");
+
+
+
+    }
+}
